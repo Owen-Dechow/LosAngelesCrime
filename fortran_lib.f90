17c800f22646b84f2103722d8a8a919e321113ce
Owen Dechow <101993353+Owen-Dechow@users.noreply.github.com>
2023-08-09 22:36:53 -0400

    Update documentation

--- fortran_lib.f90
+++ fortran_lib.f90
@@ -2,18 +2,28 @@ module lib
 contains
 
     subroutine get_vic_sex_count(load_data, m, f, x)
+        implicit none
+
+        ! Arguments
         character(*), dimension(:, :), intent(in) :: load_data
+
+        ! Return values
         integer, intent(out) :: f, m, x
+
+        ! Variables
         character(:), dimension(:), allocatable :: sex_list
         integer :: idx
         character :: char
 
+        ! Set counters to 0
         f = 0
         m = 0
         x = 0
 
+        ! Extract column
         sex_list = load_data(:, 13)
 
+        ! Count elements
         do idx = 1, size(sex_list)
             char = sex_list(idx)
             select case (char)
@@ -29,26 +39,32 @@ contains
 
     subroutine get_vic_age_data(load_data, mean, mode)
         implicit none
+
+        ! Arguments
         character(*), dimension(:, :), intent(in) :: load_data
+
+        ! Return values
         double precision, intent(out) :: mean, mode
+
+        ! Variables
         character(:), dimension(:), allocatable :: char_age_list
-        integer :: count = 0, idx = 0
-        integer, dimension(:), allocatable :: age_list
+        integer :: count = 0, idx = 0, age
         integer, dimension(0:200) :: mode_count = 0
         integer :: tmp(1)
 
-        ! Pull data
+        ! Extract column
         char_age_list = load_data(:, 12)
-        allocate (age_list(size(char_age_list)))
 
         ! Get mean/mode
         mean = 0
         do idx = 1, size(char_age_list)
-            read (char_age_list(idx), "(i20)") age_list(idx)
+            read (char_age_list(idx), "(i20)") age
             count = count + 1
-            mean = mean + age_list(idx)
-            mode_count(age_list(idx)) = mode_count(age_list(idx)) + 1
+            mean = mean + age
+            mode_count(age) = mode_count(age) + 1
         end do
+
+        ! Set return values
         mean = mean/count
         tmp = maxloc(mode_count)
         mode = tmp(1) - 1
@@ -120,23 +136,33 @@ contains
     subroutine get_most_common(load_data, col, max_unique, max_out_length, n_out, most_common)
         implicit none
 
-        integer :: max_out_length, n_out
+        ! Arguments
+        integer, intent(in) :: max_out_length, n_out, col, max_unique
         character(*), dimension(:, :), intent(in) :: load_data
-        integer, intent(in) :: col, max_unique
+
+        ! Return values
         character(max_out_length), dimension(n_out), intent(out) :: most_common
+
+        ! Variables
         character(max_out_length), dimension(max_unique) :: key_pair
         integer, dimension(max_unique) :: value_pair
         character(:), dimension(:), allocatable :: check_col
         integer :: idx, idx2, location(1), fill_idx = 1, tmp(1)
         character(:), allocatable :: str
 
+        ! Preset key/value pair
         key_pair = ""
         value_pair = 0
 
+        ! Extract column
         check_col = load_data(:, col)
+
+        ! Count elements
         do idx = 1, size(check_col)
+            ! Get element to count
             str = check_col(idx)
 
+            ! Find counter location
             location = 0
             do idx2 = 1, size(key_pair)
                 if (key_pair(idx2) == str) then
@@ -144,6 +170,7 @@ contains
                 end if
             end do
 
+            ! Increment counter
             if (str /= "") then
                 if (location(1) > 0) then
                     value_pair(location(1)) = value_pair(location(1)) + 1
@@ -155,6 +182,7 @@ contains
             end if
         end do
 
+        ! Harvest highest counters
         do idx = 1, size(most_common)
             tmp = maxloc(value_pair)
             value_pair(tmp(1)) = -1
@@ -166,24 +194,34 @@ contains
     subroutine get_most_common_counted(load_data, col, max_unique, max_out_length, n_out, most_common, counted)
         implicit none
 
-        integer :: max_out_length, n_out
+        ! Arguments
+        integer, intent(in) :: max_out_length, n_out, col, max_unique
         character(*), dimension(:, :), intent(in) :: load_data
-        integer, intent(in) :: col, max_unique
+
+        ! Return values
         character(max_out_length), dimension(n_out), intent(out) :: most_common
         integer, dimension(n_out), intent(out) :: counted
+
+        ! Variables
         character(max_out_length), dimension(max_unique) :: key_pair
         integer, dimension(max_unique) :: value_pair
         character(:), dimension(:), allocatable :: check_col
         integer :: idx, idx2, location(1), fill_idx = 1, tmp(1)
         character(:), allocatable :: str
 
+        ! Preset key/value pair
         key_pair = ""
         value_pair = 0
 
+        ! Extract column
         check_col = load_data(:, col)
+
+        ! Count elements
         do idx = 1, size(check_col)
+            ! Get element to count
             str = check_col(idx)
 
+            ! Find counter location
             location = 0
             do idx2 = 1, size(key_pair)
                 if (key_pair(idx2) == str) then
@@ -191,6 +229,7 @@ contains
                 end if
             end do
 
+            ! Increment counter
             if (str /= "") then
                 if (location(1) > 0) then
                     value_pair(location(1)) = value_pair(location(1)) + 1
@@ -202,6 +241,7 @@ contains
             end if
         end do
 
+        ! Harvest highest counters
         do idx = 1, size(most_common)
             tmp = maxloc(value_pair)
             most_common(idx) = key_pair(tmp(1))
